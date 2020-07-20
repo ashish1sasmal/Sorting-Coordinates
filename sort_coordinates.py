b7b9a7c882eea5d1665ff0db904bf248c84d47f2
ashish1sasmal <ashishsasmal1@gmail.com>
2020-07-21 02:55:38 +0530

    results

--- sort_coordinates.py
+++ sort_coordinates.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
 import imutils
+import time
+from imutils import contours
+import sys
 
 cv2.namedWindow("Sort",cv2.WINDOW_NORMAL)
 
-img = cv2.imread("Test/objects4.jpg")
+img = cv2.imread("Test/"+sys.argv[1])
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (5,5), 0)
@@ -17,15 +20,22 @@ cnts = imutils.grab_contours(cnts)
 out = img.copy()
 
 print(len(cnts))
+cnts,_ = contours.sort_contours(cnts)
+ind=1
 for (i,c) in enumerate(cnts):
-	if cv2.contourArea(c)>85:
+	if cv2.contourArea(c)>55:
 		box = cv2.minAreaRect(c)
 		box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
 		box = np.array(box, dtype="int")
+		print(box)
+		cv2.putText(out, "Object #{}".format(ind),(box[0][0],box[0][1]),cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.6, (0,0, 255),1)
 		cv2.drawContours(out, [box], -1, (0, 255, 0), 2)
+		ind+=1
+
     # cv2.drawContours(out, [c], -1, (0,255,0), 0)
 
 cv2.imshow("Sort",out)
+cv2.imwrite("Results/result_"+sys.argv[1],out)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
